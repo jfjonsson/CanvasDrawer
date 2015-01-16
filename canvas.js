1490f308348affeb6a5b1a664104c7f485c28a6a
Jón Freysteinn Jónsson <nonni95@gmail.com>
2015-01-16 13:14:29 +0000

    started implementing tool and color select

--- canvas.js
+++ canvas.js
@@ -3,6 +3,28 @@ $(function() {
     var cv = document.getElementById("paintView");
   	var ctx = cv.getContext("2d");
   	ctx.canvas.width  = window.innerWidth - 30;
-  	ctx.canvas.height = window.innerHeight - 30;    
-});
+  	ctx.canvas.height = window.innerHeight - 30; 
 
+  	ctx.fillStyle="#EFEFEF";
+  	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  	function clearCanvas() {
+		ctx.clearRect ( 0 , 0 , ctx.canvas.width, ctx.canvas.height );
+		ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	} 
+
+	$(".newCanvas").click( function( event ) {
+		console.log("clearing");
+		clearCanvas();
+	});
+
+
+	$(".toolSelect").click( function ( event ) {
+		console.log("Clicked " + this.innerHTML);
+	});
+
+
+	$(".colorSelect").click( function ( event ) {
+		console.log("Clicked " + this.innerHTML);
+	});
+});
