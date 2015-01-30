404d5144201c1edfd43664261a591f0d9c5b171c
Jón Freysteinn Jónsson <nonni95@gmail.com>
2015-01-30 11:31:20 +0000

    added erase functionality

--- canvas.js
+++ canvas.js
@@ -21,25 +21,47 @@ $(function() {
 		clearCanvas();
 	});
 
+	window.addEventListener("keydown", function(ev) {
+		if(ev.keyCode === 8 && document.activeElement !== 'text') {
+        	ev.preventDefault();
+        	drawing.deleteElement();
+    	} else if (ev.keyCode === 38) {
+    		//UP arrow
+    		drawing.moveElement(0, -10);
+    	} else if (ev.keyCode === 40) {
+    		//DOWN arrow
+    		drawing.moveElement(0, 10);
+    	} else if (ev.keyCode === 37) {
+    		//LEFT arrow
+    		drawing.moveElement(-10, 0);
+    	} else if (ev.keyCode === 39) {
+    		//RIGHT arrow
+    		drawing.moveElement(10, 0);
+    	}
+	});
 
-	$(".toolSelect").click( function ( event ) {
+	$(".toolSelect").click( function (ev) {
 		drawing.tool = this.getAttribute("data-toolType");
 	});
 
-	$(".saveCanvas").click( function( event ) {
-		//TODO: save current canvas
+	$(".saveCanvas").click( function(ev) {
+		//TODO: save canvas
 	});
 
-	$(".undo").click( function( event ) {
+	$(".loadCanvas").click( function(ev) {
+		//TODO: load canvas
+	});
+
+	$(".undo").click( function( ev ) {
 		drawing.undo();
 	});
 
-	$(".redo").click( function( event ) {
+	$(".redo").click( function( ev ) {
 		drawing.redo();
 	});
 
 
-	$(".colorSelect").click( function ( event ) {
+	$(".colorSelect").click( function ( ev ) {
 		drawing.color = this.getAttribute("data-toolColor");
 	});
     console.log( "ready!" );
@@ -49,7 +71,8 @@ $(function() {
 		canvasRedoStack: [],
 		selectedId: null,
 		color: "#282828",
-		tool: "circle",
+		selectColor: "#A0A0A0",
+		tool: "pen",
 		undo: function() {
 			if(this.canvasStack.length > 0) {
 				this.canvasRedoStack.push(this.canvasStack.pop());
@@ -65,8 +88,6 @@ $(function() {
 		drawElements: function() {
 			clearCanvas();
 			for (var i = 0; i < this.canvasStack.length; ++i) {
-				//console.log("Drawing elem " + i);
-				//console.log(this.canvasStack[i].color);
 				this.canvasStack[i].draw(cntxt);
 			};
 		},
@@ -80,8 +101,27 @@ $(function() {
 					return;
 				}
 			};
+			this.selectedId = null;
+		},
+		moveElement: function(x, y) {
+			if(this.selectedId !== null) {
+				this.canvasStack[this.selectedId].moveSelected(x, y);
+				this.drawElements();
+			}
+		},
+		deleteElement: function(x, y) {
+			if (x !== undefined && y !== undefined){
+				console.log("erasing cords");
+				this.selectElement(x, y);
+				this.deleteElement();
+			} else if(this.selectedId !== null) {
+				console.log("erasing");
+				this.canvasRedoStack.push(this.canvasStack[this.selectedId]);
+				this.canvasStack.splice(this.selectedId, 1);
+				selectedId = null;
+				this.drawElements();
+			} 
 		}
-
 	};
 
 	var isDrawing = false;
@@ -91,22 +131,24 @@ $(function() {
 	var drawing_starty = 0;
 
 	$("#paintView").mousedown(function (ev) {
+		console.log("mouse down");
 		drawing_startx = ev.pageX - this.offsetLeft;
 		drawing_starty = ev.pageY - this.offsetTop;
 		isDrawing = true;
 		if(drawing.tool === "select") {
-			if(drawing.selectedId === null) {
-				drawing.selectElement(drawing_startx, drawing_starty);
-			} 
+			drawing.selectElement(drawing_startx, drawing_starty);
+		} else if (drawing.tool === "pen") {
+			drawing.canvasStack.push(new Pen(drawing_startx, drawing_starty));
+		} else if (drawing.tool === "erase") {
+			drawing.deleteElement(drawing_startx, drawing_starty);
 		}
 	});
 
 	$("#paintView").mousemove(function (ev) {
 		if(isDrawing === true) {
 			console.log("mouse move");
-			drawing.drawElements();
-			var x = ev.pageX - this.offsetLeft;
-			var y = ev.pageY - this.offsetTop;
+			var x = ev.pageX - this.offsetLeft,
+			    y = ev.pageY - this.offsetTop;
 
 			if(drawing.tool === "line") {
 				lineHelper(x, y);
@@ -114,10 +156,16 @@ $(function() {
 				circleHelper(x, y);
 			} else if(drawing.tool === "rect") {
 				rectHelper(x, y);
-			} else if(drawing.tool === "select" && drawing.selectedId != null) {
-				selectHelper(x, y);
+			} else if(drawing.tool === "select" && drawing.selectedId !== null) {
+				var xdev = x - drawing_startx,
+				ydev = y - drawing_starty;
+				drawing.moveElement(xdev, ydev);
 				drawing_startx = ev.pageX - this.offsetLeft;
 				drawing_starty = ev.pageY - this.offsetTop;
+			} else if(drawing.tool === "pen") {
+				drawing.canvasStack[drawing.canvasStack.length - 1].addCords(x, y);
+			} else if (drawing.tool === "erase") {
+				drawing.deleteElement(x, y);
 			}
 		}
 	});
@@ -129,28 +177,13 @@ $(function() {
 		isDrawing = false;
 
 		if(drawing.tool === "line") {
-			var newLine = new Line(x, y);
-			drawing.canvasStack.push(newLine);
+			drawing.canvasStack.push(new Line(x, y));
 		} else if(drawing.tool === "circle") {
-			var newCircle = new Circle(x, y);
-			drawing.canvasStack.push(newCircle);
+			drawing.canvasStack.push(new Circle(x, y));
 		} else if(drawing.tool === "rect") {
-			var newRect = new Rect(x, y);
-			drawing.canvasStack.push(newRect);
-		} else if(drawing.tool === "select") {
-			drawing.selectedId = null;
+			drawing.canvasStack.push(new Rect(x, y));
 		}
 	});
-
-	function selectHelper(x, y) {
-		var xdev = x - drawing_startx;
-		var ydev = y - drawing_starty;
-		console.log(drawing.selectedId);
-		drawing.canvasStack[drawing.selectedId].startx += xdev;
-		drawing.canvasStack[drawing.selectedId].starty += ydev;
-		drawing.canvasStack[drawing.selectedId].endx += xdev;
-		drawing.canvasStack[drawing.selectedId].endy += ydev;
-	}
 
 	var Shape = Base.extend({
 		constructor: function(x, y, color, type) {
@@ -161,56 +194,98 @@ $(function() {
 			this.color = color;
 			this.type = type;
 			this.selected = false;
-			this.lineCords = [];
 		},
 		atPoint: function(x, y) {
-			if(this.type === "circle") {
-				var dx = (this.endx - this.startx),
-				dy = (this.endy - this.starty),
-				radius = Math.sqrt(dx * dx + dy * dy),
-				xmn = this.startx - radius - 10,
-				xmx = this.startx + radius + 10,
-				ymn = this.starty - radius - 10,
-				ymx = this.starty + radius + 10;
-			} else {
-				var xmn = Math.min(this.startx, this.endx) - 10,
-				xmx = Math.max(this.startx, this.endx) + 10,
-				ymn = Math.min(this.starty, this.endy) - 10,
-				ymx = Math.max(this.starty, this.endy) + 10;
-			}
+			var xmn = Math.min(this.startx, this.endx) - 10,
+			xmx = Math.max(this.startx, this.endx) + 10,
+			ymn = Math.min(this.starty, this.endy) - 10,
+			ymx = Math.max(this.starty, this.endy) + 10;
 			return (x >= xmn) && (x <= xmx) 
 			&& (y >= ymn) && (y <= ymx);
 		},
 		drawSelect: function() {
-			cntxt.strokeStyle = "#FF0000";
+			cntxt.strokeStyle = drawing.selectColor;
 			cntxt.beginPath();
-			if(this.type === "circle") {
-				var dx = (this.endx - this.startx),
-				dy = (this.endy - this.starty),
-				radius = Math.sqrt(dx * dx + dy * dy);
-				cntxt.rect(this.startx - radius - 10, this.starty - radius - 10, radius * 2 + 20, radius * 2 + 20);
-			} else {
-				cntxt.rect(this.startx - 10, this.starty - 10, 
-				this.endx - this.startx + 20, this.endy - this.starty + 20);
-			}
+			cntxt.rect(this.startx - 10, this.starty - 10, 
+			this.endx - this.startx + 20, this.endy - this.starty + 20);
 			cntxt.stroke();
+		},
+		moveSelected: function(x, y) {
+			this.startx += x;
+			this.starty += y;
+			this.endx += x;
+			this.endy += y;
 		}
 	});
 
-	function penHelper(x, y) {
-
-	}
+	
+	var Point = Base.extend({
+		constructor: function(x, y) {
+			this.x = x;
+			this.y = y;
+		}
+	});
 
 	var Pen = Shape.extend({
-		constructor: function() {
-
+		constructor: function(x, y) {
+			this.base(x, y, drawing.color, "pen");
+			this.cords = [];
+			this.cords.push(new Point(x, y));
+			this.xmx = 0;
+			this.xmn = 100000;
+			this.ymx = 0;
+			this.ymn = 100000;
 		},
 		draw: function(cntxt) {
-
+			cntxt.strokeStyle = this.color;
+			cntxt.beginPath();
+			cntxt.moveTo(this.cords[0].x, this.cords[0].y);
+			for (var i = 1; i < this.cords.length; i++) {
+					cntxt.lineTo(this.cords[i].x, this.cords[i].y);
+			}
+			cntxt.stroke();
+		},
+		drawSelect: function() {
+			cntxt.strokeStyle = drawing.selectColor;
+			cntxt.beginPath();
+			cntxt.rect(this.xmn - 10, this.ymn - 10, 
+				this.xmx - this.xmn + 20, this.ymx - this.ymn + 20);
+			cntxt.stroke();
+		},
+		atPoint: function(x, y) {
+			return (x >= this.xmn) && (x <= this.xmx) 
+			&& (y >= this.ymn) && (y <= this.ymx);
+		},
+		addCords: function(x, y) {
+			this.cords.push(new Point(x, y));
+			drawing.drawElements();
+			if(x > this.xmx) {
+				this.xmx = x;
+			}
+			if(x < this.xmn) {
+				this.xmn = x;
+			}
+			if(y > this.ymx) {
+				this.ymx = y;
+			}
+			if(y < this.ymn) {
+				this.ymn = y;
+			}
+		},
+		moveSelected: function(x, y) {
+			for (var i = 0; i < this.cords.length; i++) {
+				this.cords[i].x += x;
+				this.cords[i].y += y;
+			};
+			this.xmx += x;
+			this.xmn += x;
+			this.ymx += y;
+			this.ymn += y;
 		}
 	});
 
 	function lineHelper(x, y) {
+		drawing.drawElements();
 		cntxt.strokeStyle = drawing.color;
 		cntxt.beginPath();
 		cntxt.moveTo(drawing_startx, drawing_starty);
@@ -234,6 +309,7 @@ $(function() {
 	});
 
 	function circleHelper(x, y){
+		drawing.drawElements();
 		cntxt.strokeStyle = drawing.color;
 		cntxt.beginPath();
 		var dx = Math.abs(drawing_startx - x);
@@ -257,10 +333,30 @@ $(function() {
 			var radius = Math.sqrt(dx * dx + dy * dy);
 			cntxt.arc(this.startx, this.starty, radius, 0, 2 * Math.PI, false);
 			cntxt.stroke();
+		},
+		drawSelect: function() {
+			cntxt.strokeStyle = drawing.selectColor;
+			cntxt.beginPath();
+			var dx = (this.endx - this.startx),
+			dy = (this.endy - this.starty),
+			radius = Math.sqrt(dx * dx + dy * dy);
+			cntxt.rect(this.startx - radius - 10, this.starty - radius - 10, radius * 2 + 20, radius * 2 + 20);
+			cntxt.stroke();
+		},
+		atPoint: function(x, y) {
+			var dx = (this.endx - this.startx),
+				dy = (this.endy - this.starty),
+				radius = Math.sqrt(dx * dx + dy * dy),
+				xmn = this.startx - radius - 10,
+				xmx = this.startx + radius + 10,
+				ymn = this.starty - radius - 10,
+				ymx = this.starty + radius + 10;
+			return (x >= xmn) && (x <= xmx) && (y >= ymn) && (y <= ymx);
 		}
 	});
 
 	function rectHelper(x, y){
+		drawing.drawElements();
 		cntxt.strokeStyle = drawing.color;
 		cntxt.beginPath();
 		cntxt.rect(drawing_startx, drawing_starty, 
@@ -279,9 +375,6 @@ $(function() {
 			cntxt.stroke();
 		}
 	});
-
-
-
 });
 
 
